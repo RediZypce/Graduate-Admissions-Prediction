726c67e4a5eb9105fb89bee27c917f3903e1d21b
Redi Zypce <109640560+RediZypce@users.noreply.github.com>
2023-11-02 20:58:21 -0400

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow	import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import layers
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.metrics import r2_score
